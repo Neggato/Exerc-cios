3ed99785848552f52ed4918b95e2bd3de955da72
jotave <jvfelix86@gmail.com>
2025-06-19 17:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Course;
+using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Entre com a altura e largura do retângulo:");
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + x.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + x.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
